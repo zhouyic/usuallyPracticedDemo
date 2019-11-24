5ef19958e4d208f53855d0895bf9a2555015a540
转角遇见你 <592519679@qq.com>
2019-11-24 22:04:26 +0800

    kafka生产者

--- zyc_parent/zyc_demo/src/test/java/com/zyc/demo/ZycDemoApplicationTests.java
+++ zyc_parent/zyc_demo/src/test/java/com/zyc/demo/ZycDemoApplicationTests.java
@@ -1,12 +1,9 @@
 package com.zyc.demo;
 
-import com.alibaba.fastjson.util.IOUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import java.io.InputStream;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
