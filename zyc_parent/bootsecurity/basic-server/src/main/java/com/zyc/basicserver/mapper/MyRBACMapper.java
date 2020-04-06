b4ad0e38396fde8bf9d039b563d596ce89777ac1
转角遇见你 <592519679@qq.com>
2020-04-06 16:10:03 +0800

    springsecurity代码提交3

---
+++ zyc_parent/bootsecurity/basic-server/src/main/java/com/zyc/basicserver/mapper/MyRBACMapper.java
@@ -0,0 +1,14 @@
+package com.zyc.basicserver.mapper;
+
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+/**
+ * @author zhouyicai
+ * @Description: TODO
+ * @date 2020/4/5 9:41
+ */
+public interface MyRBACMapper {
+    List<String> findUriByUserName(@Param("username") String username);
+}
